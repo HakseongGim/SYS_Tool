42416e2be80a8c4546ff79c4d28ffa2d4d5e2bf7
HakseongGim <56946648+HakseongGim@users.noreply.github.com>
2021-07-29 19:30:00 +0900

    엑셀 저장기능 추가
    엑셀에 시트별로 저장하는 코드 추가

--- 풍력발전단지 점검량 계산/WT_inspection_time_cal.py
+++ 풍력발전단지 점검량 계산/WT_inspection_time_cal.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import WF_name_separator
-import os
 from datetime import datetime, timedelta
 
 API_key = 'xrJZ8HroohTzA4twNYzuzANO61JIKQYz2ZIFgnzW'
@@ -45,7 +44,7 @@ def WT_number_separator (df_data):
         elm = datetime.strptime(i, '%H:%M:%S') + timedelta(hours=9)   
         Time.append(elm.time())
     Time = pd.Series(Time)
-    
+#-----------------------------------------------------------------------------
     a = []
     b = []
     for i in range(len(num_Start)):
@@ -54,45 +53,28 @@ def WT_number_separator (df_data):
  
 #-----------------------------------------------------------------------------
     Time_interval = []
-    poket = []
+ 
     for p1, p2 in zip(num_Start, num_End):
         elm = datetime.strptime(Time_str[p2],'%H:%M:%S') - datetime.strptime(Time_str[p1],'%H:%M:%S')
          # elm = Time[p2] - Time[p1]
-        Time_interval.append(elm)
+        Time_interval.append(str(elm))
  
     
     total_data = pd.DataFrame(zip(Date_str[num_Start],WF_name[num_Start], WT_num[num_Start], Time[a], Time[b], Time_interval))
     total_data.columns = ['점검일','단지','호기', '시작시간','종료시간','점검시간']
-     
-    poket.append(total_data)
-    return print(poket)
+    
+    
+    
+    return total_data
     
 #-----------------------------------------------------------------------------
 
-# writer = pd.ExcelWriter('test.xlsx', engine='xlsxwriter')
-for i in range(len(df_re)):
-    WT_number_separator(df_re[i])
-    
+writer = pd.ExcelWriter('Result.xlsx', engine='xlsxwriter')
 
-
-# with pd.ExcelWrither('test.xlsx') as writer:
-
+for i in range(len(df_re)): 
+    sh_name = WT_number_separator(df_re[i]).loc[0, '단지']
+    WT_number_separator(df_re[i]).to_excel(writer, sheet_name = str(sh_name), index = False)
     
+writer.save()
     
-    
-# folder = 'WT_inspection'  
-#     base_dir = 'C:\\Users\\HakSeong Gim\\Desktop\\{}'.format(folder)
-#     file_nm = '1일 점검량.xlxs'
-#     xlxs_dir = os.path.join(base_dir, file_nm)
-    
-#     with pd.ExcelWriter(xlxs_dir) as writer:
-#         total_data.to_excel(writer, encoding ='utf-8-sig', index=False, sheet_name ='1'  )
-        
-
-# WT_number_separator(df_re[0])
-# folder = 'WT_inspection'
-# save_name = '점검시간.csv'
-
-# total_data.to_csv('C:\\Users\\tkdcn\\OneDrive\\바탕 화면\\{}/{}'.format(folder, save_name), encoding = 'utf-8-sig', index=False)
-# # total_data.to_csv('C:\\Users\\HakSeong Gim\\Desktop\\{}/{}'.format(folder, save_name), encoding = 'utf-8-sig', index=False)
 
