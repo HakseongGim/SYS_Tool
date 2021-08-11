6cad41e237c97360b6e2139f568e4391e5081c60
HakseongGim <56946648+HakseongGim@users.noreply.github.com>
2021-08-11 13:10:55 +0900

    2차 수정
    1. 검출 기준 수정 1 -> 0.04
    2. 폴더 내 이미지 파일 존재 유무 확인에 따른 메시지 출력
    3. 이미지의 매칭결과 저장
    4. 이미지 내 매칭된 부분을 잘라낸 후 후처리하여 저장

--- 이미지 처리/image_moment.py
+++ 이미지 처리/image_moment.py
@@ -1,42 +1,54 @@
 import os
 import cv2
+import pathlib
+import sys
 
-path = 'C:/Users/HakSeong Gim/.spyder-py3/이미지 매칭'
-os.chdir(path)
-files = os.listdir(path)
+def load_image(img_path) :
+    files = os.listdir(img_path)
+    img_files = list(filter(lambda x: x.find('jpg') != -1 or x.find('png') != -1 or x.find('JPG') != -1 , files))
+      
+    if not img_files :
+        print("이미지 파일이 존재하지 않습니다.")
+        sys.exit()
+        
+    for elm in img_files :
+        if 'Template' in elm :
+            Temp = elm
+            img_files.remove(Temp)
+        else:
+            Temp = None
 
-img_files = list(filter(lambda x: x.find('.jpg') != -1 or x.find('png') != -1 or x.find('JPG') != -1 , files))
-img_files_src = []
+    if not Temp :
+        print("Template 이미지 파일이 존재하지 않습니다.")
+        sys.exit()
 
-for file in img_files :
-    if 'Template' in file :
-        Temp  = file
-    else:
-        img_files_src.append(file)        
-           
-
-def matcing_moment_in_image (Temp, img_files_src) :    
-
-    obj = cv2.imread(Temp, cv2.IMREAD_GRAYSCALE)    
+    if not img_files :
+        print("매칭할 이미지 파일이 존재하지 않습니다.")
+        sys.exit()
+        
+            
+    return Temp, img_files
+       
+def matching_moment_in_image (Temp, img_files_src) :    
+    
+    obj = cv2.imread(Temp, cv2.IMREAD_GRAYSCALE)      
     src = cv2.imread(img_files_src, cv2.IMREAD_GRAYSCALE)
-    result = cv2.imread(img_files_src)
-
- #-------------------------객체의-외곽 검출-------------------------------------
-    _, obj_bin = cv2.threshold(obj, 128, 255, cv2.THRESH_BINARY_INV)
-# 임계처리 함수 retval, dst 형태로 출력하나 retval은 제외 (_,)
     
-    obj_contours, _ = cv2.findContours(obj_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    _, obj_bin = cv2.threshold(obj, 127, 255, cv2.THRESH_BINARY_INV)
+    # 임계처리 함수 retval, dst 형태로 출력하나 retval은 제외 (_,)
     
+    obj_contours, _ = cv2.findContours(obj_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  
     obj_pts = obj_contours[0]
-    
-
-#------------------------입력 영상 분석----------------------------------------
+        
     _, src_bin = cv2.threshold(src, 128,255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(src_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
+      
     dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
-#-----------------------------------------------------------------------------
+
+    area_list = []
+    
     for pts in contours:
+       
         if cv2.contourArea(pts) < 1000:
             continue
          
@@ -45,21 +57,39 @@ def matcing_moment_in_image (Temp, img_files_src) :
         
         # 모양 비교
         dist = cv2.matchShapes(obj_pts, pts, cv2.CONTOURS_MATCH_I3, 0)
+        cv2.putText(dst, str(round(dist, 4)), (rc[0], rc[1] - 3), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 1, cv2.LINE_AA)
         
-        if dist < 0.1:        
+        
+        if dist < 0.04:        
             cv2.rectangle(dst, rc, (0,0,255),2)
-            area = rc 
-       
-            return result[area[1]:area[1]+area[3], area[0]:area[0]+area[2]] 
-            
-img_H = 520
-img_V = 280
+            area_list.append(rc)
+     
+    cv2.imwrite(img_files_src + "_matching_result" + ".jpg", dst)
+                
+    return area_list
+ 
+img_path = pathlib.Path().absolute()
+Temp, img_files = load_image(img_path)
 
-for i in range(len(img_files_src)) :
-    cropped_img = matcing_moment_in_image(Temp, img_files_src[i])
-    im_resize = cv2.resize(cropped_img, dsize=(img_H, img_V), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite("Result_" + img_files_src[i] + ".png", im_resize)
-
-
+for i in range(len(img_files)) :
+    crop_area = matching_moment_in_image(Temp, img_files[i])
+    
+    for j in range(len(crop_area)) :
+    
+        org_img = cv2.imread(img_files[i])
+        obj = cv2.imread(Temp)
+        
+        V, H, _ = obj.shape
+   
+        org_crop_img = org_img[crop_area[j][1]:crop_area[j][1]+crop_area[j][3], crop_area[j][0]:crop_area[j][0]+crop_area[j][2]]
+        org_crop_img_resize = cv2.resize(org_crop_img, dsize=(H, V), interpolation=cv2.INTER_CUBIC)
+        
+        re_crop_img = org_crop_img_resize[int(V*0.25):int(V*0.73), int(H*0.34):int(H*0.65)]
+        
+        V_c, H_c, _ = re_crop_img.shape
+        re_crop_img_resize = cv2.resize(re_crop_img, dsize=(int(H_c*3), int(V_c*3)), interpolation=cv2.INTER_CUBIC)
+        
+        cv2.imwrite(img_files[i] + "_Result_" + str(j+1) + ".jpg", re_crop_img_resize)
+         
 cv2.waitKey(0)
 cv2.destroyAllWindows()
